2f7a7a072240b03c7cd3acc9843e26794eabf0ba
Aryan16032004 <aryan16062004@gmail.com>
2024-09-05 12:18:15 +0530

    utils

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -3,6 +3,6 @@ class ApiResponse{
         this.statusCode=statusCode
         this.data=data
         this.message=message
-        this.statusCode=statusCode < 400
+        this.isSuccessful=statusCode < 400
     }
 }
